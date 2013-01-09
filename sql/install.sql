ff981e4731a5716aacbf0317627787fb96414901
Xmk <xmk@ars-team.ru>
2013-01-09 19:42:25 +0600

    Fix topic_view fk

--- sql/install.sql
+++ sql/install.sql
@@ -233,7 +233,7 @@ ALTER TABLE `prefix_forum_topic`
 -- Constraints for table `prefix_forum_topic_view`
 --
 ALTER TABLE `prefix_forum_topic_view`
-	ADD CONSTRAINT `prefix_forum_topic_view_fk` FOREIGN KEY (`topic_id`) REFERENCES `prefix_topic` (`topic_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+	ADD CONSTRAINT `prefix_forum_topic_view_fk` FOREIGN KEY (`topic_id`) REFERENCES `prefix_forum_topic` (`topic_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `prefix_forum_readonly`
